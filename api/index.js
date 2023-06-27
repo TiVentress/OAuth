13720749395b8d80f0e885250c2639ba4618466d
Tiago Lara <tilara20@gmail.com>
2023-06-27 06:45:36 -0300

    Add pastas api e back-end

---
+++ api/index.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+import qs from "query-string";
+
+
+function redirectToGitHub() {
+    const GITHUB_URL = 'https://github.com/login/oauth/authorize';
+    const params ={
+        response_type: 'code',
+        scope: 'user',
+        client_id: process.env.CLIENT_ID,
+        redirect_uri: process.env.REDIRECT_URL,
+        state: 'test-t5'
+    }
+    const queryStrings = qs.stringify(params);
+    const authURL = `${GITHUB_URL}?${queryStrings}`;
+    window.location.href = authURL
+}
+
+window.onload = async () => {
+    document.querySelector(".login").addEventListener("click", redirectToGitHub);
+    const { code } = qs.parseUrl(window.location.href).query;
+    if (code) {
+        try {
+            const response = await axios.post(`${process.env.BACK_END_URL}/login`, {code});
+            const user = (response).data;
+            console.log(user);
+        } catch (error) {
+            alert("O login falhou!");
+            console.log("error", err);
+        }
+    }
+}    
